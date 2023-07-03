8271bd8f9de7e26d0323b53a2f32711449b8e845
Ramón Orraca <dev@ramonorraca.com>
2023-07-02 20:41:31 -0600

    Add GTM connection for analytics.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    GOOGLE_TAG_MANAGER_ID: process.env.GOOGLE_TAG_MANAGER_ID
+  }
 }
 
 export default nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  const tagManagerArgs = {
+    gtmId: process.env.GOOGLE_TAG_MANAGER_ID,
+  }
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <>
       <Component {...pageProps} />
