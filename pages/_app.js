81cf5d20f3fc6591672b1354ea09617e224ce579
ramon-san <ramon.orraca@correo.uia.mx>
2023-10-06 14:58:40 +0800

    Fix _app.js to avoid 'exhaustive-deps rule' warning.

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import TagManager from 'react-gtm-module';
 import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  const tagManagerArgs = {
-    gtmId: process.env.GOOGLE_TAG_MANAGER_ID,
-  };
-
   useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
+    if (process.env.GOOGLE_TAG_MANAGER_ID) {
+      const tagManagerArgs = {
+        gtmId: process.env.GOOGLE_TAG_MANAGER_ID,
+      };
+
+      TagManager.initialize(tagManagerArgs);
+    }
   }, []);
 
   return (
